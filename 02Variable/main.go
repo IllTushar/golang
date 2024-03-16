4e910f809ea4196e7c6aaac5282bc76ab32fceea
IllTushar <gtushar7668@gmail.com>
2024-03-17 00:41:51 +0530

    first commit

---
+++ 02Variable/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "Tushar"
+	fmt.Println(username)
+	fmt.Printf("The Variable type is: %T\n", username)
+
+	var isLogin bool = true
+	fmt.Println(isLogin)
+	fmt.Printf("The Variable type is: %T\n", isLogin)
+}
